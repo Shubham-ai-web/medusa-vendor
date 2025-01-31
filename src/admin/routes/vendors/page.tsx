d0b221fc4309b1948431c31011e97091067da762
Shubham Sonwane <shubham@seepossible.com>
2025-01-31 17:37:17 +0530

    *

--- src/admin/routes/vendors/page.tsx
+++ src/admin/routes/vendors/page.tsx
@@ -2,11 +2,10 @@ import { defineRouteConfig } from "@medusajs/admin-sdk";
 import { TagSolid } from "@medusajs/icons";
 import {
   Container,
-  DataTable,
   Heading,
-  createDataTableColumnHelper,
   DataTablePaginationState,
-  useDataTable,
+  Table,
+  Text,
 } from "@medusajs/ui";
 import { useQuery } from "@tanstack/react-query";
 import { sdk } from "../../lib/sdk";
@@ -29,29 +28,6 @@ export type VendorsResponse = {
 };
 
 const VendorsPage = () => {
-  const columnHelper = createDataTableColumnHelper<Vendor>();
-
-  const columns = [
-    columnHelper.accessor("id", {
-      header: "ID",
-    }),
-    columnHelper.accessor("email", {
-      header: "Email",
-    }),
-    columnHelper.accessor("first_name", {
-      header: "First Name",
-    }),
-    columnHelper.accessor("last_name", {
-      header: "Last Name",
-    }),
-    columnHelper.accessor("phone", {
-      header: "Phone",
-    }),
-    columnHelper.accessor("address", {
-      header: "Address",
-    }),
-  ];
-
   const limit = 15;
   const [pagination, setPagination] = useState<DataTablePaginationState>({
     pageSize: limit,
@@ -71,30 +47,39 @@ const VendorsPage = () => {
       }),
     queryKey: [["vendors", limit, offset]],
   });
-  console.log(data, "data");
-
-  const table = useDataTable({
-    columns,
-    data: data?.vendors || [],
-    getRowId: (row) => row.id,
-    rowCount: data?.count || 0,
-    isLoading,
-    pagination: {
-      state: pagination,
-      onPaginationChange: setPagination,
-    },
-  });
 
   return (
-    <Container className="divide-y p-0">
-      <DataTable instance={table}>
-        <DataTable.Toolbar className="flex flex-col items-start justify-between gap-2 md:flex-row md:items-center">
-          <Heading>Vendors</Heading>
-          <VendorCreateDrawer refetch={refetch} />
-        </DataTable.Toolbar>
-        <DataTable.Table />
-        <DataTable.Pagination />
-      </DataTable>
+    <Container className="flex flex-col p-0 overflow-hidden">
+      <div className="p-6 flex justify-between">
+        <Heading>Vendors</Heading>
+        <VendorCreateDrawer refetch={refetch} />
+      </div>
+
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>ID</Table.HeaderCell>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Email</Table.HeaderCell>
+            <Table.HeaderCell>Phone</Table.HeaderCell>
+            <Table.HeaderCell>Address</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {isLoading && <Text className="text-center">Loading...</Text>}
+          {data?.vendors.map((vendor) => (
+            <Table.Row key={vendor.id}>
+              <Table.Cell>{vendor.id}</Table.Cell>
+              <Table.Cell>
+                {vendor.first_name} {vendor.last_name}
+              </Table.Cell>
+              <Table.Cell>{vendor.email}</Table.Cell>
+              <Table.Cell>{vendor.phone}</Table.Cell>
+              <Table.Cell>{vendor.address}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </Container>
   );
 };
